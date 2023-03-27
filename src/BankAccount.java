60bce82ad9a5c13c4caf4a223f22873d935d7df2
Marcin <gasiorek.mail@gmail.com>
2023-03-27 11:10:47 +0200

    synchronized method

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -8,11 +8,17 @@ public class BankAccount {
         this.balance = initialBalance;
     }
 
-    public void deposit(double amount) {
-        balance += amount;
+    public double getBalance() {
+        return balance;
     }
 
+    public synchronized void deposit(double amount) {
+        balance += amount;
+    }
+//alternative way if there is more code
     public void withdraw(double amount) {
-        balance -= amount;
+        synchronized (this) {
+            balance -= amount;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         final BankAccount account = new BankAccount("12345-678", 1000);
+        System.out.println("Balance before Threads is: " + account.getBalance());
         Thread trThread1 = new Thread() {
             @Override
             public void run() {
                 account.deposit(300);
                 account.withdraw(50);
+                System.out.println("balance Thread 1  is: " + account.getBalance());
             }
         };
         Thread trThread2 = new Thread() {
@@ -14,8 +16,17 @@ public class Main {
             public void run() {
                 account.deposit(203.75);
                 account.withdraw(100);
+                System.out.println("balance Thread 2 is " + account.getBalance());
             }
         };
+//        another way of creating Thread using runnable:
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                account.deposit(203.75);
+//                account.withdraw(100);
+//            }
+//        }).start();
         trThread1.start();
         trThread2.start();
     }
